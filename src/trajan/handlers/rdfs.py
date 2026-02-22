Fixed mass multipliation bug + added area normalization of vdos + fixed structure factor calculation

src/trajan/handlers/rdfs.py
@@ -216,7 +216,7 @@ class RDFS(BASE):
             oscillations = self.T_r - T_0
 
             if self.calc_scatter:
-                self.q_grid = np.arange(0.01, self.sQmax + self.dq, self.dq)
+                self.q_grid = np.arange(2 * np.pi / self.cutoff, self.sQmax + self.dq, self.dq)
 
                 Q_2d = self.q_grid[:, np.newaxis]
                 r_2d = self.edges[np.newaxis, :]
@@ -225,7 +225,7 @@ class RDFS(BASE):
                 integral = scipy.integrate.simpson(integrand, x=self.edges, axis=-1)
 
                 self.i_Q = integral / self.q_grid
-                self.S_Q = 1.0 + (self.i_Q / 0.01 * (average_scat**2))
+                self.S_Q = 1.0 + (self.i_Q / (0.01 * (average_scat**2)))
 
             #Lorch Broadening
             if self.broaden:

src/trajan/handlers/vdos.py
@@ -68,7 +68,7 @@ class VDOS(BASE):
                 self.autocount.append(0)
 
             current_velocities = self.extract_columns("vx", "vy", "vz")
-            current_masses = self.extract_column("mass")
+            current_masses = self.extract_columns("mass").T[0]
             self.history.append(current_velocities)
 
             for tau, past_velocities in enumerate(reversed(self.history)):
@@ -119,6 +119,9 @@ class VDOS(BASE):
 
         #Conversion from Hz to cm^-1
         self.frequencies = np.fft.rfftfreq(self.padding_total, d = self.timestep * self.lag_step) / c_cm_s
+
+        area = np.trapz(self.vDOS, self.frequencies)
+        self.vDOS / area
 
     def write(self):
 